8b02ea1988686b81fd4717a9d9d39057f2b3d6f8
ChelaLorenchia <chela.lorenchia277@gmail.com>
2024-12-05 03:48:43 +0700

    update frontend itinery,destinasi, nav

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -20,8 +20,31 @@ router.get('/user/:id', verifyToken, userController.getUserById);
 router.put('/user/:id', verifyToken, checkPermission, userController.updateUser);
 router.delete('/user/:id', verifyToken, checkPermission, userController.deleteUser);
 
+// Rute untuk Destionation
+router.get('/destination', destinationController.getAllDestinations);
+router.get('/destination/:id', destinationController.getDestinationById);
+router.get('/destinations', verifyToken, destinationController.getAllDestinationsWithAuth);
+router.get('/destinations/:id', verifyToken, destinationController.getDestinationByIdWithAuth);
+router.post('/destination', verifyToken, upload, destinationController.addDestination);
+router.put('/destination/:id', verifyToken, upload, destinationController.updateDestination);
+router.delete('/destination/:id', verifyToken, destinationController.deleteDestination);
+
 // Rute untuk Favorite
 router.get('/favorites/:userId', verifyToken, favoriteController.getFavorites);
 router.post('/favorites', verifyToken, favoriteController.toggleFavorite);
+
+// Rute untuk Itinerary
+router.get('/itineraries', itineraryController.getAllItinerariesWithDays);
+router.get('/itineraries/:id', itineraryController.getItineraryWithDaysById);
+router.get('/itinerary', verifyToken, itineraryController.getAllItinerariesWithAuthAndDays);
+router.get('/your-itinerary', verifyToken, itineraryController.getItinerariesByUser);
+router.get('/itinerary/:id', verifyToken, itineraryController.getItineraryWithDaysByIdWithAuth);
+router.post('/itineraries', verifyToken, itineraryController.addItinerary);
+router.put('/itineraries/:id', verifyToken, itineraryController.updateItineraryWithDays);
+router.delete('/itineraries/:id', verifyToken, itineraryController.deleteItineraryWithDays);
+
+// Rute untuk Day
+router.get('/days/:itineraryId', dayController.getDays);
+router.post('/days', verifyToken, dayController.addDay);
 
 module.exports = router;
