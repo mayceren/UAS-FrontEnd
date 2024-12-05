0a51a88f66f3dd819915e855cff921d2fe038313
lusykar1in4 <161692437+lusykar1in4@users.noreply.github.com>
2024-12-05 21:14:33 +0700

    update

--- frontend/src/pages/profile/updateProfile/updateProfile.controller.js
+++ frontend/src/pages/profile/updateProfile/updateProfile.controller.js
@@ -60,48 +60,4 @@ app.controller('UpdateProfileController', function ($scope, $http, $window, $sta
             $scope.user.password = '';
         }
     };
-
-    $scope.deleteUser = function (userId) {
-        console.log("delete user id : ", userId);
-        
-        // Swal.fire({
-        //     title: 'Are you sure?',
-        //     text: 'Do you want to delete this account?',
-        //     icon: 'warning',
-        //     showCancelButton: true,
-        //     confirmButtonText: 'Yes, delete it!',
-        //     cancelButtonText: 'No, keep it',
-        //     reverseButtons: true
-        // }).then((result) => {
-        //     if (result.isConfirmed) {
-        //         // Jika user memilih untuk menghapus
-        //         $http.delete(`http://localhost:3001/api/user/${userId}`, {
-        //             headers: {
-        //                 'Authorization': `Bearer ${token}`
-        //             }
-        //         }).then(function (response) {
-        //             Swal.fire(
-        //                 'Deleted!',
-        //                 'Your Account has been deleted.',
-        //                 'success'
-        //             );
-        //             $state.go('home')
-        //         }).catch(function (error) {
-        //             console.error('Error deleting account:', error);
-        //             Swal.fire(
-        //                 'Error!',
-        //                 'Failed to delete the account.',
-        //                 'error'
-        //             );
-        //         });
-        //     } else {
-        //         // Jika user memilih untuk membatalkan
-        //         Swal.fire(
-        //             'Cancelled',
-        //             'Your itinerary is safe :)',
-        //             'info'
-        //         );
-        //     }
-        // });
-    };
 });
