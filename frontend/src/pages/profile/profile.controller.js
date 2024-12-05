069f4dc38ccb64597081a5274f27af8e61c07a56
lusykar1in4 <161692437+lusykar1in4@users.noreply.github.com>
2024-12-05 17:07:23 +0700

    update

--- frontend/src/pages/profile/profile.controller.js
+++ frontend/src/pages/profile/profile.controller.js
@@ -27,6 +27,52 @@ app.controller('ProfileController', function ($scope, $http, AuthService) {
       });
   };
 
+  
+
+  $scope.deleteUser = function (userId) {
+    console.log("delete user id : ", userId);
+    
+    // Swal.fire({
+    //     title: 'Are you sure?',
+    //     text: 'Do you want to delete this account?',
+    //     icon: 'warning',
+    //     showCancelButton: true,
+    //     confirmButtonText: 'Yes, delete it!',
+    //     cancelButtonText: 'No, keep it',
+    //     reverseButtons: true
+    // }).then((result) => {
+    //     if (result.isConfirmed) {
+    //         // Jika user memilih untuk menghapus
+    //         $http.delete(`http://localhost:3001/api/user/${userId}`, {
+    //             headers: {
+    //                 'Authorization': `Bearer ${token}`
+    //             }
+    //         }).then(function (response) {
+    //             Swal.fire(
+    //                 'Deleted!',
+    //                 'Your Account has been deleted.',
+    //                 'success'
+    //             );
+    //             $state.go('home')
+    //         }).catch(function (error) {
+    //             console.error('Error deleting account:', error);
+    //             Swal.fire(
+    //                 'Error!',
+    //                 'Failed to delete the account.',
+    //                 'error'
+    //             );
+    //         });
+    //     } else {
+    //         // Jika user memilih untuk membatalkan
+    //         Swal.fire(
+    //             'Cancelled',
+    //             'Your itinerary is safe :)',
+    //             'info'
+    //         );
+    //     }
+    // });
+  };
+
   // Fungsi logout dari AuthService
   $scope.logout = function () {
     Swal.fire({
--- frontend/src/pages/profile/updateProfile/updateProfile.controller.js
+++ frontend/src/pages/profile/updateProfile/updateProfile.controller.js
@@ -29,12 +29,6 @@ app.controller('UpdateProfileController', function ($scope, $http, $window, $sta
             name: $scope.user.name,
             email: $scope.user.email,
         };
-
-        // Tambahkan password hanya jika diisi
-        if ($scope.user.password) {
-            updateData.password = $scope.user.password;
-        }
-
         $http
             .put(`http://localhost:3001/api/user/${userId}`, updateData, {
                 headers: { Authorization: `Bearer ${token}` },
@@ -51,57 +45,5 @@ app.controller('UpdateProfileController', function ($scope, $http, $window, $sta
                         ? error.data.message
                         : 'Error updating profile. Please try again.';
             });
-    };
-
-    // Fungsi untuk menampilkan atau menyembunyikan input password
-    $scope.togglePasswordField = function () {
-        $scope.showPasswordField = !$scope.showPasswordField;
-        if (!$scope.showPasswordField) {
-            $scope.user.password = '';
-        }
-    };
-
-    $scope.deleteUser = function (userId) {
-        console.log("delete user id : ", userId);
-        
-        // Swal.fire({
-        //     title: 'Are you sure?',
-        //     text: 'Do you want to delete this account?',
-        //     icon: 'warning',
-        //     showCancelButton: true,
-        //     confirmButtonText: 'Yes, delete it!',
-        //     cancelButtonText: 'No, keep it',
-        //     reverseButtons: true
-        // }).then((result) => {
-        //     if (result.isConfirmed) {
-        //         // Jika user memilih untuk menghapus
-        //         $http.delete(`http://localhost:3001/api/user/${userId}`, {
-        //             headers: {
-        //                 'Authorization': `Bearer ${token}`
-        //             }
-        //         }).then(function (response) {
-        //             Swal.fire(
-        //                 'Deleted!',
-        //                 'Your Account has been deleted.',
-        //                 'success'
-        //             );
-        //             $state.go('home')
-        //         }).catch(function (error) {
-        //             console.error('Error deleting account:', error);
-        //             Swal.fire(
-        //                 'Error!',
-        //                 'Failed to delete the account.',
-        //                 'error'
-        //             );
-        //         });
-        //     } else {
-        //         // Jika user memilih untuk membatalkan
-        //         Swal.fire(
-        //             'Cancelled',
-        //             'Your itinerary is safe :)',
-        //             'info'
-        //         );
-        //     }
-        // });
     };
 });
